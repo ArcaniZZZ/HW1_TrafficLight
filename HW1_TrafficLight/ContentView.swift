e762f1fada29bf48780ee218d997350f833ccfe3
Alex <a.zubach@finch-melrose.com>
2022-09-08 19:05:26 +0300

    Initial Commit

---
+++ HW1_TrafficLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW1_TrafficLight
+//
+//  Created by Alexander Zubach on 08.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW1_TrafficLight/HW1_TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW1_TrafficLightApp.swift
+//  HW1_TrafficLight
+//
+//  Created by Alexander Zubach on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW1_TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
